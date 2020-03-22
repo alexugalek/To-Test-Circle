b88fefdac2760fb15b221a35d640bfb7e13a2d8f
alexugalek <alex_ugalek@mail.ru>
2020-03-22 12:06:36 +0300

    Fix errors

--- src/simple.py
+++ src/simple.py
@@ -2,7 +2,9 @@ import math
 
 
 def check_simple(num):
-    for divider in range(2, math.floor(num**0.5)):
+    if num == 0:
+        return False
+    for divider in range(2, math.floor(num**0.5) + 1):
         if not (num % divider):
             return False
     return True
